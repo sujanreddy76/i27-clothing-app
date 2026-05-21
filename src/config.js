f3ce71cf641c66a38bc8f062c6b8936f3b8b6025
sujan <sujan.reddy.76@gmail.com>
2026-05-21 21:09:26 +0530

    edited config.js

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 const produtsServiceLocalHost = 'http://localhost:8132/api'
 const usersServiceLocalHost = 'http://localhost:8232/api'
 
-const produtsServiceDevHost = 'http://104.198.52.21:30263/api' 
-const usersServiceDevHost = 'http://104.198.52.21:31904/api'
+const produtsServiceDevHost = 'http://34.10.232.83:5132/api' 
+const usersServiceDevHost = 'http://34.10.232.83:5232/api'
 
 const produtsServiceTestHost = ''
 const usersServiceTestHost = ''
